b76d8d96b837e19162bfe0a78e0570bb46d51bdb
Mayara Oliveira <mayoliva0931@gmail.com>
2022-08-25 18:03:57 -0300

    criando componente Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://i.pinimg.com/564x/59/c3/a6/59c3a67e0c2697881b61913746c07a2b.jpg" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
