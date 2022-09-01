2fd56d7e6ebb719b4976e94b2705c212cb1c224b
Mayara Oliveira <mayoliva0931@gmail.com>
2022-09-01 18:05:55 -0300

    fazendo o login e cadastro funcionarem corretamente, consumindo a API

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import './App.css';
@@ -10,17 +11,24 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <Navbar />
-      <div style={{ minHeight: '100vh' }}>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </div>
-      <Footer />
-    </Router>
-  );
+    <Navbar />
+    <Routes>
+
+      <Route path="/" element={<Login  />} />
+
+      <Route path="/home" element={<Home />} />
+
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+
+    </Routes>
+    <Footer />
+  </Router>
+
+);
 }
 
 export default App;
+
+
